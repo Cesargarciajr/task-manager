401e8378d4850c94067715a449cd3fedf4e46a6c
Cesargarciajr <>
2023-09-30 16:47:00 +0000

    Add base template and flask framework

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,9 +7,8 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
 
-
-from taskmanager import routes # noqa
+from taskmanager import routes  # noqa
