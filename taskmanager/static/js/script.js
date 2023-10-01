756d352bfcd7ba45726bfdbd90541ab049ff7ff4
Cesargarciajr <>
2023-10-01 10:02:33 +0000

    Add 'ass task' function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // sidenav initialization
-  let sidenav = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(sidenav);
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // Date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
