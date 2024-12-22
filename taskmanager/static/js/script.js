5fd0fcc10caa507cdcc81ce15bb0d9f7f578fe7c
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-12-22 18:30:20 +0000

    updates css and js

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,21 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-    // Date picker
-    let datepicker = document.querySelectorAll('.datepicker');
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
-    // Select initialization
-    let selects = document.querySelectorAll('select');
+    // select initialization
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    // Collapsible initialization
-    let collapsible = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(collapsible);
-
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
