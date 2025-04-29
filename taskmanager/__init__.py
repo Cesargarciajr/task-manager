ebb377ee1d81176e9feac875a07a068ef220b61c
Cesar Garcia <cesargarcia.elder@gmail.com>
2025-04-29 12:50:29 +0000

    Migrate to Neon database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -15,7 +15,11 @@ else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+    if "sslmode" not in uri:
+        uri += "?sslmode=require"
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+
 
 db = SQLAlchemy(app)
 
